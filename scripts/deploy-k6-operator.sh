d9deb05c0496597957bcb1431f6359c4dc40ad0b
Harrison (Hieu) Van <Hieu_Van@mckinsey.com>
2021-06-29 18:57:17 +0700

    fix k6-operator

--- scripts/deploy-k6-operator.sh
+++ scripts/deploy-k6-operator.sh
@@ -9,3 +9,15 @@ set -eoux pipefail
 
 cd operator
 make deploy
+make install
+
+kubectl get serviceaccounts k6-operator-k6-operator --namespace=k6-operator-system --output=yaml | sed 's/k6-operator-k6-operator/k6-operator/g' | kubectl apply --filename -
+kubectl get rolebindings k6-operator-leader-election-rolebinding --namespace=k6-operator-system --output=yaml | sed 's/k6-operator-k6-operator/k6-operator/g' | kubectl apply --filename -
+kubectl get roles k6-operator-leader-election-role --namespace=k6-operator-system --output=yaml | sed 's/k6-operator-k6-operator/k6-operator/g' | kubectl apply --filename -
+
+kubectl get clusterrolebindings k6-operator-manager-rolebinding --output=yaml | sed 's/k6-operator-k6-operator/k6-operator/g' | kubectl apply --filename -
+kubectl get clusterrolebindings k6-operator-proxy-rolebinding --output=yaml | sed 's/k6-operator-k6-operator/k6-operator/g' | kubectl apply --filename -
+
+kubectl get clusterroles k6-operator-manager-role --output=yaml | sed 's/k6-operator-k6-operator/k6-operator/g' | kubectl apply --filename -
+kubectl get clusterroles k6-operator-metrics-reader --output=yaml | sed 's/k6-operator-k6-operator/k6-operator/g' | kubectl apply --filename -
+kubectl get clusterroles k6-operator-proxy-role --output=yaml | sed 's/k6-operator-k6-operator/k6-operator/g' | kubectl apply --filename -
