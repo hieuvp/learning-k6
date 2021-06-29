5d736160d6dcce96dc4f76cbf867aa507263e0bd
Harrison (Hieu) Van <Hieu_Van@mckinsey.com>
2021-06-29 15:04:05 +0700

    postman-to-k6

---
+++ scripts/postman-to-k6.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eoux pipefail
+
+trash tests/k6
+
+# Convert Postman collection to k6 test
+cd tests/postman
+postman-to-k6 collection.json \
+  --environment=environment.json \
+  --output=../k6/script.js
