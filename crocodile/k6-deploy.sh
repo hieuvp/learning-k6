d72e690e32d2a682699edc745c7418c0f7af61d1
Harrison (Hieu) Van <Hieu_Van@mckinsey.com>
2021-07-04 13:02:30 +0700

    fix crocodile

--- crocodile/k6-deploy.sh
+++ crocodile/k6-deploy.sh
@@ -7,7 +7,10 @@ kubectl create namespace k6-test
 
 kubectl create configmap crocodile-stress-test \
   --from-file=crocodile/k6-test-script.js \
-  --namespace=k6-test
+  --namespace=k6-test \
+  --output=yaml --dry-run |
+  sed "s/k6-test-script.js/test.js/" |
+  kubectl apply --filename -
 
 kubectl apply \
   --filename=crocodile/k6-resource.yml \
